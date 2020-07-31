b552e56fea521d8730af00d64f68b2b36755bffd
shortpoet <soriano.carlitos@gmail.com>
2020-07-31 12:58:52 -0500

    add iq to parity outlier

--- typescript/src/codewars/findEvenOddOutlier.ts
+++ typescript/src/codewars/findEvenOddOutlier.ts
@@ -38,3 +38,23 @@ export function findOutlier3(integers: number[]): number {
   integers.forEach(num => num % 2 === 0 ? evenList.push(num) : oddList.push(num));
   return oddList.length === 1 ? oddList[0] : evenList[0];
 }
+
+export function iqTest(numbers: string): number {
+  function findOutlier(integers: number[]): number {
+    const odds = integers.filter(v => v % 2);
+    const evens = integers.filter(v => !(v % 2));
+  
+    return odds.length === 1 ? odds[0] : evens[0];
+  }
+  const digits = numbers.split(" ").map(x => +x);
+  
+  return digits.indexOf(findOutlier(digits)) + 1;
+}
+
+export function iqTest2(numbers: string): number {
+  const isEven = (n: number) => n % 2 === 0;
+  const isEvenNumbers = numbers.split(" ").map(v => isEven(+v));
+  const oddsCount = isEvenNumbers.filter(v => v === true).length;
+  const evensCount = isEvenNumbers.filter(v => v === false).length;
+  return isEvenNumbers.indexOf(oddsCount < evensCount) + 1;
+}
