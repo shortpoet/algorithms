5bb51cbe2713e4d4064ffa6d230afd37db990dcc
shortpoet <soriano.carlitos@gmail.com>
2020-11-25 15:01:57 -0600

    solve wordPattern

--- typescript/jest.config.js
+++ typescript/jest.config.js
@@ -4,7 +4,16 @@ module.exports = {
   ],
   "testMatch": [
     "**/__tests__/**/*.+(ts|tsx|js)",
-    "**/?(*.)+(spec|test).+(ts|tsx|js)"
+    "**/?(*.)+(spec|test).+(ts|tsx|js)",
+    // "<rootDir>/src/**/__tests__/**/*.[jt]s?(x)",
+    // "<rootDir>/src/**/*(*.)@(spec|test).[tj]s?(x)",
+    // '**/?(*.)(spec).js?(x)',
+    // '**/?(*.)(test).js?(x)',
+    // "**\\__tests__\\**\\*.+(ts|tsx|js)",
+    // "**\\?(*.)+(spec|test).+(ts|tsx|js)",
+    // '**/__tests__/*.+(ts|tsx|js)',
+    // "<rootDir>/src/**/*.(test).{js,jsx,ts,tsx}",
+    // "<rootDir>/src/**/?(*.)(spec|test).{js,jsx,ts,tsx}"
   ],
   "transform": {
     "^.+\\.(ts|tsx)$": "ts-jest"
--- typescript/src/codewars/wordPattern.ts
+++ typescript/src/codewars/wordPattern.ts
@@ -1,4 +1,14 @@
 export const wordPattern =
   (word: string): string => { 
-    return "";
+    const cipher = word
+      .toLowerCase()
+      .split('')
+      .filter((v, i, a) => a.indexOf(v) == i)
+    const out = word
+      .toLowerCase()
+      .split('')
+      .map(x => cipher.indexOf(x))
+      .join('.');
+    console.log(out);
+    return out;
   }
