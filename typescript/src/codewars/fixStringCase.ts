0cf8be90eb427909492c97618d6e7141984421f2
shortpoet <soriano.carlitos@gmail.com>
2020-11-26 14:59:00 -0600

    add fixStringCase

--- typescript/src/codewars/fixStringCase.ts
+++ typescript/src/codewars/fixStringCase.ts
@@ -15,3 +15,13 @@ export const fixStringCase =
       ? s.toLowerCase()
         : s.toUpperCase();
   }
+
+// https://www.codewars.com/kata/5b180e9fedaa564a7000009a/solutions/typescript
+
+  export function solve(s: string) {
+    let uppercaseCount = s.split('').filter(letter => letter === letter.toUpperCase()).length;
+    return uppercaseCount > s.length / 2 ? s.toUpperCase() : s.toLowerCase();
+  }
+  export function solve2(s: string) {
+    return s.replace(/[a-z]/g, '').length > s.length / 2 ? s.toUpperCase() : s.toLowerCase();
+  }
