947b4bd63e26bbec6f34f757a01d469f6f932419
shortpoet <soriano.carlitos@gmail.com>
2020-12-03 10:00:12 -0600

    solve sumPairs

--- typescript/src/codewars/sumPairs.ts
+++ typescript/src/codewars/sumPairs.ts
@@ -1,4 +1,53 @@
 export const sumPairs =
-  (ints: number[], s: number): [number, number] => {
-    return [0,0];
+  (ints: number[], s: number): [number, number] | undefined => {
+    const evens: number[] = [];
+    const odds: number[] = [];
+    const sumPolarity: ('even' | 'odd') = s % 2 == 0
+      ? 'even'
+      : 'odd'
+    let results: [number, number] = [0, 0];
+    let found = false;
+    for (let i = 0; i < ints.length; i++) {
+      const num = ints[i];
+      const numPolarity: ('even' | 'odd') = num % 2 == 0
+      ? 'even'
+      : 'odd'
+      if (numPolarity != sumPolarity) {
+        const finding: (number | undefined) = odds.find(o => o + num == s);
+        if (finding != undefined) {
+          results[0] = finding;
+          results[1] = num;
+          found = true;
+          break;
+        }
+      } else {
+        const finding: (number | undefined) = evens.find(e => e + num == s);
+        if (finding != undefined) {
+          results[0] = finding;
+          results[1] = num;
+          found = true;
+          break;
+        }
+      }
+      if (numPolarity == 'even') {
+        evens.push(num);
+      } else {
+        odds.push(num);
+      }
+
+    }
+    return found
+      ? results
+      : undefined;
   };
+
+// https://www.codewars.com/kata/54d81488b981293527000c8f/solutions/typescript
+
+// 
+export function sumPairs0(ints: number[], s: number): [number, number] | void {  
+  const seen = new Set();
+  for (const n of ints) {
+    if (seen.has(s - n)) return [s - n, n];
+    seen.add(n);
+  }
+}
--- typescript/src/codewars/tests/sumPairs.spec.ts
+++ typescript/src/codewars/tests/sumPairs.spec.ts
@@ -11,27 +11,27 @@ l8: number[] = [5, 9, 13, -3];
 
 describe("sumPairs.ts", function() {
   test("Basic: ["+l1+"] should return [1, 7] for sum = 8", function() {
-    expect(sumPairs(l1, 8)).toBe([1, 7]);
+    expect(sumPairs(l1, 8)).toEqual([1, 7]);
   });
   test("Negatives: ["+l2+"] should return [0, -6] for sum = -6", function() {
-    expect(sumPairs(l2, -6)).toBe([0, -6]);
+    expect(sumPairs(l2, -6)).toEqual([0, -6]);
   });
   test("No Match: ["+l3+"] should return undefined for sum = -7", function() {
-    expect(sumPairs(l3, -7)).toBe(undefined);
+    expect(sumPairs(l3, -7)).toEqual(undefined);
   });
   test("First Match From Left: ["+l4+"] should return [1, 1] for sum = 2 ", function() {
-    expect(sumPairs(l4, 2)).toBe([1, 1]);
+    expect(sumPairs(l4, 2)).toEqual([1, 1]);
   });
   test("First Match From Left REDUX!: ["+l5+"] should return [3, 7] for sum = 10 ", function() {
-    expect(sumPairs(l5, 10)).toBe([3, 7]);
+    expect(sumPairs(l5, 10)).toEqual([3, 7]);
   });
   test("Duplicates: ["+l6+"] should return [4, 4] for sum = 8", function() {
-    expect(sumPairs(l6, 8)).toBe([4, 4]);
+    expect(sumPairs(l6, 8)).toEqual([4, 4]);
   });
   test("Zeroes: ["+l7+"] should return [0, 0] for sum = 0", function() {
-    expect(sumPairs(l7, 0)).toBe([0, 0]);
+    expect(sumPairs(l7, 0)).toEqual([0, 0]);
   });
   test("Subtraction: ["+l8+"] should return [13, -3] for sum = 10", function() {
-    expect(sumPairs(l8, 10)).toBe([13, -3]);
+    expect(sumPairs(l8, 10)).toEqual([13, -3]);
   });
 });
